c5a11b55b9a4349dedf372eaa769cffdf369747a
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-03 15:51:47 +0500

    work complete

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ import postRoutes from './routes/posts.js'
 const app = express();
 
 
-app.use('/posts', postRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL =
-  "mongodb://socialmediaapp:socialmediaappdb@cluster0-shard-00-00.t5qza.mongodb.net:27017,cluster0-shard-00-01.t5qza.mongodb.net:27017,cluster0-shard-00-02.t5qza.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-5pdjfb-shard-0&authSource=admin&retryWrites=true&w=majority";
+  "mongodb://@cluster0-shard-00-00.t5qza.mongodb.net:27017,cluster0-shard-00-01.t5qza.mongodb.net:27017,cluster0-shard-00-02.t5qza.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-5pdjfb-shard-0&authSource=admin&retryWrites=true&w=majority";
 const PORT = process.env.port || 5000;
 
 mongoose
