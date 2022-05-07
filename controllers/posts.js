2bbbec818b3a50b6cb95b0608d408d918c297ce4
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-07 18:35:46 +0500

    work complete

--- controllers/posts.js
+++ controllers/posts.js
@@ -39,10 +39,22 @@ export const updatePost = async (req, res) => {
 export const deletePost = async (req, res) => {
   const { id } = req.params;
   
-  if (!mongoose.Types.ObjectId.isValid(_id))
+  if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("No Post with That Id");
 
   await PostMessage.findByIdAndRemove(id);
   res.json({message: 'post deleted successfully'});
+
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No Post with That Id");
+
+  const post=await PostMessage.findById(id);
+  const updatedPost= await PostMessage.findByIdAndUpdate(id, {likeCount : post.likeCount + 1 }, {new: true})
+  res.json(updatedPost);
 
 };
