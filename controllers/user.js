8907bbbeadf3106388a97dc7c7c282528075e128
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-16 16:53:35 +0500

    work complete

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,22 @@ import jwt from 'jsonwebtoken'
 import User from '../models/user.js'
 
 export const signin= async (req,res)=>{
+    const {email,password} = req.body;
+    try {
+        const existingUser = await User.findOne({ email })
+
+        if(!existingUser) return res.status(404).json({message: 'User Does not exist'});
+
+        const isPasswordCorrect= await bcrypt.compare(password, existingUser.password)
+
+        if(!isPasswordCorrect) return res.status(404).json({message: 'Password is incorrect'})
+
+        const token = jwt.sign({email: existingUser.email, id: existingUser._id},'test',{expiresIn: "1h"})
+
+        res.status(200).json({result: existingUser, token})
+    } catch (error) {
+        res.status(500).json({message: "Something went wrong"})
+    }
 
 }
 
