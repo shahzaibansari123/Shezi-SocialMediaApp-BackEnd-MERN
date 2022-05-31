cd5b82a949eaf4e1736d253c357de49edf94a411
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-31 20:25:15 +0500

    work complete

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getPost, getPosts,getPostsBySearch, createPost, updatePost,deletePost, likePost} from '../controllers/posts.js' 
+import {getPost, getPosts,getPostsBySearch, createPost, updatePost,deletePost, likePost, commentPost} from '../controllers/posts.js' 
 
 
 import auth from '../middleware/auth.js';
@@ -18,6 +18,7 @@ router.post('/',auth,createPost)
 router.patch('/:id',auth,updatePost)
 router.delete('/:id',auth,deletePost)
 router.patch('/:id/likePost',auth,likePost)
+router.post('/:id/commentPost',auth,commentPost)
 
 export default router
 
