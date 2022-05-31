d60d8e83104cec2aaa73bc4508bd6b9ad8acdd1e
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-31 23:26:49 +0500

    work complete

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-// import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
 
 const app = express();
-const CONNECTION_URL = "mongodb://admin:admin@cluster0-shard-00-00.t5qza.mongodb.net:27017,cluster0-shard-00-01.t5qza.mongodb.net:27017,cluster0-shard-00-02.t5qza.mongodb.net:27017/SocialMediaApp?ssl=true&replicaSet=atlas-5pdjfb-shard-0&authSource=admin&retryWrites=true&w=majority"
+// const CONNECTION_URL = ""
 const PORT = process.env.PORT || 5000;
-// dotenv.config();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -27,7 +27,7 @@ res.send("App Is Running")
 
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
